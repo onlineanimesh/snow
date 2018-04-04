4bd126e970603a0478076302efc2f4c883b65ee7
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2018-04-04 10:06:02 +0530

    create batch ajax added

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -39,74 +39,31 @@ function getQueryString() {
 	
 	if(pageId == 'login'){
 		//auth();
-		domRenderLogin();
-	}
+		/**
+		*
+		* Login
+		*
+		*/
+		var elBtnLogin = '#btnLogin';
+		var elFrmLogin = '#frmLogin';
+		
+		$(elBtnLogin).on('click',doLogin);
+	}	
+	
 	if(pageId == 'operation'){
 		requiredAuth();
-		domRenderOperation();
-	}
- }
- 
- function requiredAuth(){
-	 if(sessionStorage.getItem('sess_username')== null || sessionStorage.getItem('sess_username') == ''){
-		window.location.href = "index.html"; 
-	 }
- }
- 
- 
- function domRenderLogin(){
-	console.log("renderLogin");
-	/**
-	*
-	* Login
-	*
-	*/
-	var elBtnLogin = '#btnLogin';
-	var elFrmLogin = '#frmLogin';
-	$(elBtnLogin).on('click',doLogin);
-	
-	function doLogin(e){
-		e.preventDefault();
-		var postUsername = $("#username").val();
-		var postPassword = $("#password").val();
-		var xhr = new Ajax();
-		xhr.type = 'get';
-		xhr.url = 'mock_data/user.json';
-		xhr.data = {username: postUsername, password: postPassword};
-		var promise = xhr.init();
-
-		promise.done(function (data) {
-			//console.log(data);
-			$.each(data, function(i,user){
-				//console.log(user.username);
-				if( (user.username == postUsername) && (user.password == postPassword) ){
-					sessionStorage.setItem('sess_username',postUsername);
-					sessionStorage.setItem('sess_password',postPassword);
-					window.location.href = "operation.html";
-				}
-			});
-		});
-		promise.done(function (data) {
-			//do more
-		});
-		promise.done(function (data) {
-			//do another task
-		});
-		promise.fail(function () {
-			//show failure message
-		});
-		promise.always(function () {
-			//always will be executed whether success or failue
-			//do some thing
-		});
-		promise.always(function () {
-			//do more on complete
-		});
 		
+		// Load Data
+		loadOperationPageData();
+		
+		// On Clicking Create Batch
+		$('#btnCreateBatch').on('click',createBatch);
 	}
  }
  
- function domRenderOperation(){	
+ 
+ 
+ function loadOperationPageData(){
 	var xhr = new Ajax();
 	xhr.type = 'get';
 	xhr.url = 'mock_data/all.json';
@@ -137,4 +94,84 @@ function getQueryString() {
 	});
  }
  
+ function doLogin(e){
+	e.preventDefault();
+	var postUsername = $("#username").val();
+	var postPassword = $("#password").val();
+	var xhr = new Ajax();
+	xhr.type = 'get';
+	xhr.url = 'mock_data/user.json';
+	xhr.data = {username: postUsername, password: postPassword};
+	var promise = xhr.init();
+
+	promise.done(function (data) {
+		//console.log(data);
+		$.each(data, function(i,user){
+			//console.log(user.username);
+			if( (user.username == postUsername) && (user.password == postPassword) ){
+				sessionStorage.setItem('sess_username',postUsername);
+				sessionStorage.setItem('sess_password',postPassword);
+				window.location.href = "operation.html";
+			}
+		});
+	});
+	promise.done(function (data) {
+		//do more
+	});
+	promise.done(function (data) {
+		//do another task
+	});
+	promise.fail(function () {
+		//show failure message
+	});
+	promise.always(function () {
+		//always will be executed whether success or failue
+		//do some thing
+	});
+	promise.always(function () {
+		//do more on complete
+	});
+	
+}
  
+function requiredAuth(){
+ if(sessionStorage.getItem('sess_username')== null || sessionStorage.getItem('sess_username') == ''){
+	window.location.href = "index.html"; 
+ }
+}
+
+function createBatch(e){
+	e.preventDefault();
+	var frm = $('#frmCreateBatch');	
+	var postData = frm.serialize();
+	var table = $('tableName');
+	var date = $('date');
+	var time = $('timepicker');
+	var regex = $('regex');
+	
+	var xhr = new Ajax();
+	xhr.type = 'get';
+	xhr.url = 'mock_data/user.json';
+	xhr.data = postData;
+	var promise = xhr.init();
+
+	promise.done(function (data) {
+		//console.log(data);
+	});
+	promise.done(function (data) {
+		//do more
+	});
+	promise.done(function (data) {
+		//do another task
+	});
+	promise.fail(function () {
+		//show failure message
+	});
+	promise.always(function () {
+		//always will be executed whether success or failue
+		//do some thing
+	});
+	promise.always(function () {
+		//do more on complete
+	});
+}
